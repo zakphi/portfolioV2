004403b8ebd9167663eb8b6d68e2d58bd5863182
Philip Zak <zakphi@gmail.com>
2017-09-26 23:40:49 -0400

    create site skeleton

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(() => {
   console.log('script loaded')
+
+  siteSkeleton = () => {
+    console.log('siteSkeleton')
+    $('<main>').prependTo('body')
+
+    $('<section>', {
+      'id': 'top'
+    }).appendTo('main')
+
+    $('<section>', {
+      'id': 'bottom'
+    }).appendTo('main')
+  }
+
+  siteSkeleton()
 })
