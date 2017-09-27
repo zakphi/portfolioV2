24f86ed31b8e222c64ff9715306b4163c84fea10
Philip Zak <zakphi@gmail.com>
2017-09-27 10:25:59 -0400

    create top content

--- js/main.js
+++ js/main.js
@@ -14,5 +14,30 @@ $(() => {
     }).appendTo('main')
   }
 
+  topSetup = () => {
+    console.log('contentSetup')
+    $('<h1>').appendTo('#top')
+
+    $('<h3>').appendTo('#top')
+
+    $('<article>').appendTo('#top')
+
+    $('<button>', {
+      'id': 'about',
+      'html': 'about'
+    }).appendTo('#top article')
+
+    $('<button>', {
+      'id': 'work',
+      'html': 'work'
+    }).appendTo('#top article')
+
+    $('<button>', {
+      'id': 'social',
+      'html': 'social'
+    }).appendTo('#top article')
+  }
+
   siteSkeleton()
+  topSetup()
 })
